7234811acb3496474172a47e8eee2e8f788812bb
waseem-medhat <wipdev.tech@gmail.com>
2024-07-23 15:03:44 +0300

    Add rough code trying `ps` and `killall`

--- cmd/pwarden/main.go
+++ cmd/pwarden/main.go
@@ -4,16 +4,53 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
-	cmd := exec.Command("ps", "-e")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	stdout := strings.Builder{}
 
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
+	var killList = []string{"Discord"}
+
+	for _, comm := range killList {
+		cmd := exec.Command("killall", "-s", "SIGINT", comm)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = &stdout
+		cmd.Stderr = os.Stderr
+
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+
+	// SEARCH HELPER (abstracts `ps` command)
+	//
+	// stdout := strings.Builder{}
+	//
+	// cmd := exec.Command("ps", "-e", "-o", "pid,comm,cmd")
+	// cmd.Stdin = os.Stdin
+	// cmd.Stdout = &stdout
+	// cmd.Stderr = os.Stderr
+	//
+	// err := cmd.Run()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// for i, pLine := range strings.Split(stdout.String(), "\n") {
+	// 	if i == 0 || pLine == "" {
+	// 		continue
+	// 	}
+	//
+	// 	pid := strings.Fields(pLine)[0]
+	// 	comm := strings.Fields(pLine)[1]
+	// 	cmd := strings.Fields(pLine)[2]
+	//
+	// 	if comm == "Discord" {
+	// 		fmt.Printf("PID: %v, COMM: %v\n", pid, comm)
+	// 		fmt.Printf("CMD: %v\n", cmd)
+	// 		fmt.Println()
+	// 	}
+	// }
 }

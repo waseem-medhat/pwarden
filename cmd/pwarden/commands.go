3417c8dcffccf1f0b50dfa25438aa3a6999502c1
waseem-medhat <wipdev.tech@gmail.com>
2024-07-24 14:56:22 +0300

    Improve command and args handling

--- cmd/pwarden/commands.go
+++ cmd/pwarden/commands.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -8,16 +9,22 @@ import (
 )
 
 type command struct {
-	callback func()
+	reqArgs  []int
+	callback func([]string) error
 }
 
 var commandMap = map[string]command{
 	"start": {
+		reqArgs:  []int{0},
 		callback: cmdStart,
+	},
+	"search": {
+		reqArgs:  []int{1},
+		callback: cmdSearch,
 	},
 }
 
-func cmdStart() {
+func cmdStart(args []string) error {
 	stdout := strings.Builder{}
 	stderr := strings.Builder{}
 
@@ -35,10 +42,43 @@ func cmdStart() {
 			if strings.Contains(stderr.String(), "no process found") {
 				continue
 			}
-
-			log.Fatal(err)
+			return err
 		}
 
 		logMessage(Success, "detected and closed "+comm)
 	}
+
+	return nil
+}
+
+func cmdSearch(args []string) error {
+	stdout := strings.Builder{}
+
+	cmd := exec.Command("ps", "-e", "-o", "pid,comm,cmd")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = &stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, pLine := range strings.Split(stdout.String(), "\n") {
+		if i == 0 || pLine == "" {
+			continue
+		}
+
+		pid := strings.Fields(pLine)[0]
+		comm := strings.Fields(pLine)[1]
+		cmd := strings.Fields(pLine)[2]
+
+		if strings.Contains(strings.ToLower(comm), strings.ToLower(args[2])) {
+			fmt.Printf("PID: %v, COMM: %v\n", pid, comm)
+			fmt.Printf("CMD: %v\n", cmd)
+			fmt.Println()
+		}
+	}
+
+	return nil
 }
--- cmd/pwarden/main.go
+++ cmd/pwarden/main.go
@@ -2,47 +2,26 @@ package main
 
 import (
 	"os"
+	"slices"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		logMessage(Error, "not enough arguments")
+		logMessage(Error, "no command provided")
 		os.Exit(1)
 	}
 
-	if command, ok := commandMap[os.Args[1]]; ok {
-		command.callback()
-	} else {
+	command, ok := commandMap[os.Args[1]]
+	if !ok {
 		logMessage(Error, os.Args[1]+" is not a command")
 		os.Exit(1)
 	}
-	// SEARCH HELPER (abstracts `ps` command)
-	//
-	// stdout := strings.Builder{}
-	//
-	// cmd := exec.Command("ps", "-e", "-o", "pid,comm,cmd")
-	// cmd.Stdin = os.Stdin
-	// cmd.Stdout = &stdout
-	// cmd.Stderr = os.Stderr
-	//
-	// err := cmd.Run()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// for i, pLine := range strings.Split(stdout.String(), "\n") {
-	// 	if i == 0 || pLine == "" {
-	// 		continue
-	// 	}
-	//
-	// 	pid := strings.Fields(pLine)[0]
-	// 	comm := strings.Fields(pLine)[1]
-	// 	cmd := strings.Fields(pLine)[2]
-	//
-	// 	if comm == "Discord" {
-	// 		fmt.Printf("PID: %v, COMM: %v\n", pid, comm)
-	// 		fmt.Printf("CMD: %v\n", cmd)
-	// 		fmt.Println()
-	// 	}
-	// }
+
+	nArgs := len(os.Args) - 2
+	if !slices.Contains(command.reqArgs, nArgs) {
+		logMessage(Error, "wrong number of arguments for "+os.Args[1])
+		os.Exit(1)
+	}
+
+	command.callback(os.Args)
 }
